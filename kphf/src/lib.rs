1a698bb47b341ed631905547574d02352de27f34
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-03-27 19:00:29 +0100

    cleanup unused variables and print statements

--- kphf/src/lib.rs
+++ kphf/src/lib.rs
@@ -54,15 +54,12 @@ pub struct KptrHash {
     mode: Mode,
     /// Number of keys per bin.
     k: usize,
-    s: usize,
     /// Number of keys.
     n: usize,
     /// Number of bins.
-    b: usize,
-    /// Metadata bits.
-    m: usize,
+    num_bins: usize,
     /// Number of buckets.
-    p: usize,
+    num_buckets: usize,
     /// Actual seeds data.
     seeds: Vec<u8>,
     /// Bump structure
@@ -70,6 +67,7 @@ pub struct KptrHash {
 }
 
 const BIN_SIZE: usize = 8;
+const BITS_PER_BUCKET: usize = 8;
 const PADDING: usize = 100;
 
 #[derive(Debug, Copy, Clone)]
@@ -93,54 +91,47 @@ impl KptrHash {
         let consensus = matches!(mode, Mode::Consensus);
         let bump = matches!(mode, Mode::LinearBump | Mode::SortBump);
 
-        // eprintln!("building..");
         let k = BIN_SIZE;
         // bits per bucket
-        let s = 8;
         let n = keys.len();
         // bins
-        let b = ((n as f32 / alpha) as usize).div_ceil(k);
+        let num_bins = ((n as f32 / alpha) as usize).div_ceil(k);
         // target metadata bits
         let m = (n as f32 * bits_per_key) as usize;
         // #buckets
-        let p = m.div_ceil(s.max(1));
-        // total bits
-        let bits = s * p;
+        let num_buckets = m.div_ceil(BITS_PER_BUCKET);
 
         // 1. sort the keys by part
         let mut keys = keys.to_vec();
         // keys.sort_unstable();
-        keys.sort_unstable_by_key(|&key| (to_part(key, p, k), key));
+        keys.sort_unstable_by_key(|&key| (to_part(key, num_buckets, k), key));
         keys.dedup();
 
         // 2. split into p parts
-        let mut part_sizes = vec![0; p];
-        let shift = 64 - p.trailing_zeros();
+        let mut part_sizes = vec![0; num_buckets];
         for key in &*keys {
-            let p = to_part(*key, p, k);
+            let p = to_part(*key, num_buckets, k);
             part_sizes[p] += 1;
         }
-        let mut part_starts = vec![0; p + 1];
-        for i in 1..=p {
+        let mut part_starts = vec![0; num_buckets + 1];
+        for i in 1..=num_buckets {
             part_starts[i] = part_starts[i - 1] + part_sizes[i - 1];
         }
 
         // 3. Sort parts by decreasing size
-        let mut perm = (0..p).collect::<Vec<_>>();
+        let mut perm = (0..num_buckets).collect::<Vec<_>>();
 
         if sort {
             perm.sort_by_key(|&i| std::cmp::Reverse(part_sizes[i]));
         }
 
         // 4. init bin sizes
-        let mut bin_sizes = vec![0u8; b + PADDING];
-        let mut seeds = vec![0u8; p + 7];
-        let mut tries = vec![0u8; p];
+        let mut bin_sizes = vec![0u8; num_bins + PADDING];
+        let mut seeds = vec![0u8; num_buckets + 7];
+        let mut tries = vec![0u8; num_buckets];
 
         let mut bumped = 0;
         let mut backtracks = 0;
-        // eprintln!("find seeds..");
-        let mut elems_done = 0;
         let mut i = 0;
 
         let mut lg: [f64; BIN_SIZE + 1] = from_fn(|i| (i as f64).log2());
@@ -148,28 +139,14 @@ impl KptrHash {
 
         let mut bumped_keys = vec![];
 
-        while i < p {
+        while i < num_buckets {
             let idx = perm[i];
             let start = part_starts[idx];
             let end = part_starts[idx + 1];
             let len = end - start;
-            // if i % 1024 == 0 {
-            //     eprintln!("bucket {i} of {p} len {len}");
-            // }
 
-            // assert_eq!(
-            //     elems_done,
-            //     bin_sizes.iter().map(|&x| x as usize).sum::<usize>() + collisions.len()
-            // );
             let num_full = bin_sizes.iter().filter(|&&x| x == k as u8).count();
 
-            // if i % 1024 == 0 {
-            //     eprintln!(
-            //         "part {idx:>10}/{p:>10}, size {len:>10}, done {elems_done:>10} ({:>7.4})",
-            //         elems_done as f32 / n as f32 * 100.0
-            //     );
-            // }
-            elems_done += len;
             let part = &keys[start..end];
             assert!(part.is_sorted());
 
@@ -184,8 +161,6 @@ impl KptrHash {
             }
             // New candidate score function:
             // Maximize the product of the number of empty slots in each bin.
-            // eprintln!("lg: {:?}", &lg);
-            // eprintln!("lg: {}", lg[0] * 2. + 1.0);
             let maybe_optimal = |counts: &[usize]| -> i64 {
                 if counts[BIN_SIZE] > 0 {
                     return i64::MAX;
@@ -195,7 +170,6 @@ impl KptrHash {
                     .enumerate()
                     .map(|(size, &cnt)| cnt as f64 * lg[BIN_SIZE - size])
                     .sum::<f64>();
-                // eprintln!("counts {counts:?} score: {q}");
                 (-q) as i64
             };
             let f = pow(7);
@@ -215,10 +189,10 @@ impl KptrHash {
                 seed_offset.to_be_bytes()
             );
 
-            for seed in 0..(1 << s) - if bump { 1 } else { 0 } {
+            for seed in 0..(1 << BITS_PER_BUCKET) - if bump { 1 } else { 0 } {
                 let mut counts = vec![0; k + 1];
                 for key in part {
-                    let bi = to_bin(*key, seed_offset + seed as u64, b);
+                    let bi = to_bin(*key, seed_offset + seed as u64, num_bins);
                     counts[(bin_sizes[bi] as usize).min(k)] += 1;
                     // update the bin_size to handle self-collisions
                     bin_sizes[bi] += 1;
@@ -226,19 +200,14 @@ impl KptrHash {
                 let score = f(&counts);
                 vals.push((counts[BIN_SIZE], score, seed));
                 for &key in part {
-                    let bi = to_bin(key, seed_offset + seed as u64, b);
+                    let bi = to_bin(key, seed_offset + seed as u64, num_bins);
                     bin_sizes[bi] -= 1;
                 }
             }
             vals.sort_by(|x, y| x.partial_cmp(y).unwrap());
             let best = vals[tries[i] as usize];
             if consensus && best.0 > 0 {
-                elems_done -= len;
                 backtracks += 1;
-                // eprintln!(
-                //     "BACKTRACK part {i} len {len} trie {} collisions {} for {:?} while best is {:?}",
-                //     tries[i], best.0, best, vals[0]
-                // );
                 // Backtrack 1 step.
                 tries[i] = 0;
                 if i > 0 {
@@ -249,20 +218,16 @@ impl KptrHash {
                     let start = part_starts[idx - 1];
                     let end = part_starts[idx];
                     let len = end - start;
-                    elems_done -= len;
                     let part = &keys[start..end];
-                    // eprintln!("Unset part {} len {len}", idx - 1);
 
-                    // eprintln!("read {}..={} to empty parent bucket", i - 1, i + 6);
                     let seed = u64::from_be_bytes(seeds[i - 1..i + 7].try_into().unwrap());
 
                     for &key in part {
-                        let bi = to_bin(key, seed, b);
+                        let bi = to_bin(key, seed, num_bins);
                         assert!(bin_sizes[bi] > 0);
                         bin_sizes[bi] -= 1;
                     }
 
-                    // eprintln!("Set {} to 0", i + 6);
                     seeds[i + 6] = 0;
                     i -= 1;
                 }
@@ -277,16 +242,10 @@ impl KptrHash {
                     // Unfixable collision found.
                     return None;
                 }
-                elems_done -= part.len();
                 bumped += part.len();
                 i += 1;
                 bumped_keys.extend_from_slice(part);
                 seeds[idx + 7] = 255;
-
-                // eprintln!(
-                //     "{i}: Bump bucket of size {}; total bumped {bumped}",
-                //     part.len()
-                // );
                 continue;
             }
 
@@ -295,25 +254,13 @@ impl KptrHash {
             seeds[idx + 7] = seed as u8;
 
             for &key in part {
-                let bi = to_bin(key, seed_offset + seed as u64, b);
+                let bi = to_bin(key, seed_offset + seed as u64, num_bins);
                 assert!( bin_sizes[bi] < k as u8, "collision at {i} size {len} seed {seed} offset {seed_offset:>0x} best {best:?} bin id {bi} bin size {}", bin_sizes[bi]);
                 bin_sizes[bi] += 1;
             }
 
             i += 1;
         }
-
-        // bin size distribution
-        let mut bsizes = vec![0; k + 1];
-        for bs in bin_sizes {
-            bsizes[bs as usize] += 1;
-        }
-        // for s in k..=k {
-        //     eprintln!(
-        //         "size {s:>2} => count  {:>6.3}%",
-        //         bsizes[s] as f32 / b as f32 * 100.0
-        //     );
-        // }
 
         let duration = start.elapsed();
         eprintln!(
@@ -329,11 +276,9 @@ impl KptrHash {
             bits_per_key,
             mode,
             k,
-            s,
             n,
-            b,
-            m,
-            p,
+            num_bins,
+            num_buckets,
             seeds,
             bumped: if bumped > 0 {
                 Some(Box::new(Self::new(
@@ -350,20 +295,20 @@ impl KptrHash {
         })
     }
 
-    fn bins(&self) -> usize {
-        self.b + self.bumped.as_ref().map_or(0, |b| b.bins())
+    pub fn bins(&self) -> usize {
+        self.num_bins + self.bumped.as_ref().map_or(0, |b| b.bins())
     }
 
-    fn get(&self, key: T) -> usize {
-        let part = to_part(key, self.p, self.k);
+    pub fn get(&self, key: T) -> usize {
+        let part = to_part(key, self.num_buckets, self.k);
         let seed = match self.mode {
             Mode::Consensus => u64::from_be_bytes(self.seeds[part..part + 8].try_into().unwrap()),
             _ => self.seeds[part + 7] as u64,
         };
         if matches!(self.mode, Mode::LinearBump | Mode::SortBump) && seed == 255 {
-            self.b + self.bumped.as_ref().unwrap().get(key)
+            self.num_bins + self.bumped.as_ref().unwrap().get(key)
         } else {
-            to_bin(key, seed, self.b)
+            to_bin(key, seed, self.num_bins)
         }
     }
 }
