934a3a4b5e79cab3394e5d854033ad303bc36fba
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-04-14 22:35:41 +0200

    move lower bound fn

--- kphf/src/bin/bench.rs
+++ kphf/src/bin/bench.rs
@@ -1,6 +1,6 @@
 #![allow(incomplete_features)]
 #![feature(adt_const_params, generic_const_exprs)]
-use kphf::{KptrHash, Mode};
+use kphf::{space_lower_bound, KptrHash, Mode};
 
 fn gen_keys(n: usize) -> Vec<u32> {
     let mut keys = std::collections::HashSet::with_capacity(n);
@@ -12,37 +12,6 @@ fn gen_keys(n: usize) -> Vec<u32> {
     let mut keys: Vec<u32> = keys.into_iter().take(n).collect();
     keys.sort_unstable();
     keys
-}
-
-/// Information-theoretic lower bound on bits/key for a static hash function
-/// with bin size k and load factor alpha.
-fn space_lower_bound(k: usize, alpha: f32) -> f32 {
-    // Rows: alpha, columns: k
-    const ALPHAS: [f32; 14] = [
-        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98, 0.99, 0.999, 1.0,
-    ];
-    const KS: [usize; 8] = [1, 2, 4, 8, 16, 32, 64, 128];
-    #[rustfmt::skip]
-    const TABLE: [[f32; 8]; 14] = [
-        [0.07466, 0.00849, 0.00022, 3e-7,     1e-12,    0.0,      0.0,      0.0     ],
-        [0.15498, 0.03112, 0.00259, 4e-5,     2e-8,     2e-14,    0.0,      0.0     ],
-        [0.24202, 0.06581, 0.00989, 5.2e-4,   3e-6,     4e-10,    0.0,      0.0     ],
-        [0.33724, 0.11221, 0.02444, 2.67e-3,  8e-5,     2e-7,     3e-12,    0.0     ],
-        [0.44269, 0.17114, 0.04833, 8.57e-3,  6.8e-4,   1.2e-5,   9e-9,     1e-14   ],
-        [0.56140, 0.24472, 0.08410, 2.095e-2, 3.18e-3,  2.0e-4,   2e-6,     6e-10   ],
-        [0.69828, 0.33704, 0.13568, 4.360e-2, 1.028e-2, 1.49e-3,  9e-5,     9e-7    ],
-        [0.86221, 0.45624, 0.21044, 8.294e-2, 2.699e-2, 6.77e-3,  1.13e-3,  9e-5    ],
-        [1.07359, 0.62179, 0.32631, 0.15418,  6.498e-2, 2.404e-2, 7.55e-3,  1.88e-3 ],
-        [1.21522, 0.73972, 0.41651, 0.21663,  0.10374,  4.552e-2, 1.815e-2, 6.47e-3 ],
-        [1.32751, 0.83742, 0.49629, 0.27699,  0.14560,  7.194e-2, 3.332e-2, 1.444e-2],
-        [1.37558, 0.88056, 0.53318, 0.30680,  0.16811,  8.765e-2, 4.339e-2, 2.036e-2],
-        [1.43271, 0.93321, 0.58010, 0.34702,  0.20111,  0.11335,  6.222e-2, 2.895e-2],
-        [1.44269, 0.94265, 0.58893, 0.35509,  0.20832,  0.11967,  6.761e-2, 3.770e-2],
-    ];
-
-    let row = ALPHAS.iter().position(|&x| x == alpha).unwrap();
-    let col = KS.iter().position(|&x| x == k).unwrap();
-    TABLE[row][col]
 }
 
 fn bench<const MODE: Mode, const K: usize>()
