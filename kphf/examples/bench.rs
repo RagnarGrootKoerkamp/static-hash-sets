2c3d40e69e5018c35330a2e3023a8a6012d31bdf
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-04-17 00:53:48 +0200

    kphf: update bench to k=4

--- kphf/examples/bench.rs
+++ kphf/examples/bench.rs
@@ -41,16 +41,25 @@ struct Result {
 }
 
 const REPEATS: usize = 1;
+const NS: [usize; 2] = [10_000_000, 100_000_000];
+const ALPHAS: [f32; 4] = [0.7, 0.8, 0.9, 0.99];
+const FACTORS: [f32; 7] = [2.5, 2.25, 2.0, 1.75, 1.5, 1.25, 1.0];
+const QUERY: bool = true;
+
+// const NS: [usize; 1] = [10_000_000];
+// const ALPHAS: [f32; 3] = [0.7, 0.8, 0.9];
+// const FACTORS: [f32; 5] = [1.6, 1.5, 1.4, 1.3, 1.2];
+// const QUERY: bool = false;
 
 fn bench<const MODE: Mode, const K: usize>()
 where
     [(); K + 1]:,
 {
-    for &n in &[10_000_000, 100_000_000] {
+    for &n in &NS {
         let keys = std::array::from_fn::<_, REPEATS, _>(|_| gen_keys(n));
-        for &alpha in &[0.7, 0.8, 0.9, 0.99] {
+        for &alpha in &ALPHAS {
             let lb = space_lower_bound(K, alpha);
-            for &factor in &[2.4, 2.2, 2.0, 1.8, 1.6, 1.4, 1.2, 1.0] {
+            for &factor in &FACTORS {
                 let target_bits_per_key = lb * factor;
 
                 for (repeat, keys) in std::iter::zip(0..REPEATS, &keys) {
@@ -65,23 +74,27 @@ where
                     let actual_alpha = n as f32 / (kphf.num_bins() as f32 * K as f32);
 
                     // Queries
-                    let start = std::time::Instant::now();
-                    let mut c = 0;
-                    for &key in keys {
-                        c += kphf.get(key);
-                    }
-                    std::hint::black_box(c);
-                    let loop_ns = start.elapsed().as_nanos() as f32 / n as f32;
+                    let mut loop_ns = 0.0;
+                    let mut throughput_ns = 0.0;
+                    if QUERY {
+                        let start = std::time::Instant::now();
+                        let mut c = 0;
+                        for &key in keys {
+                            c += kphf.get(key);
+                        }
+                        std::hint::black_box(c);
+                        loop_ns = start.elapsed().as_nanos() as f32 / n as f32;
 
-                    let start = std::time::Instant::now();
-                    let lookahead = 32;
-                    let mut c = 0;
-                    for i in 0..keys.len().saturating_sub(lookahead) {
-                        kphf.prefetch(keys[i + lookahead]);
-                        c += kphf.get(keys[i]) as usize;
+                        let start = std::time::Instant::now();
+                        let lookahead = 32;
+                        let mut c = 0;
+                        for i in 0..keys.len().saturating_sub(lookahead) {
+                            kphf.prefetch(keys[i + lookahead]);
+                            c += kphf.get(keys[i]) as usize;
+                        }
+                        std::hint::black_box(c);
+                        throughput_ns = start.elapsed().as_nanos() as f32 / n as f32;
                     }
-                    std::hint::black_box(c);
-                    let throughput_ns = start.elapsed().as_nanos() as f32 / n as f32;
 
                     let result = Result {
                         n,
@@ -108,6 +121,9 @@ where
 }
 
 fn main() {
+    bench::<{ Mode::SortBumpGreedy }, 4>();
+    bench::<{ Mode::SortBump }, 4>();
+
     bench::<{ Mode::SortBumpGreedy }, 8>();
     bench::<{ Mode::SortBump }, 8>();
 
