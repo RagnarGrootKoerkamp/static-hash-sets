93ec0caa1a5fcb51e1184e116ee8e46d2dea0921
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-04-23 14:18:31 +0200

    update plots

--- static_hash_set/eval/plot.py
+++ static_hash_set/eval/plot.py
@@ -18,99 +18,160 @@ for name in sys.argv[1:]:
             return "PhfSet<PHast+>"
         if "PtrHash" in h:
             return "PhfSet<PtrHash>"
+        if "KptrHash" in h:
+            return "kPhfSet<kPtrHash>"
         return h
 
     data["h"] = data["h"].map(cleanup_name)
     data = data[~(data["h"] == "FphMetaSet")]
+    data = data[~(data["h"] == "MapEmbed")]
+    data = data[~(data["h"] == "Hd8Set")]
+    data = data[~(data["metric"] == "prefetch")]
 
-    for metric, data in data.groupby("metric"):
-        data["label"] = data.apply(
-            lambda row: row["h"] + " (" + str(row["alpha"]) + "x)", axis=1
-        )
-        print(data.columns)
+    modes = ["all"]
+    if name == "laptop":
+        modes = ["single", "all"]
 
-        queries = ["q01", "q50", "q99"]
-        group_columns = [
-            "h",
-            "pf",
-            "threads",
-            "metric",
-            "n",
-            "alpha",
-            "label",
-            "kphf_target_bits_per_key",
-        ]
-        data = data.groupby(group_columns, as_index=False)[["build", *queries]].min()
+    for mode in modes:
+        for metric, data in data.groupby("metric"):
+            if mode == "single" and metric != "prefetch2":
+                continue
+            print(name, mode, metric)
 
-        labels = data["label"].unique()
-        print(labels)
-        palette = sns.color_palette(n_colors=len(labels))
-        label_color = {
-            "FxHashSet": "red",
-            "U64HashSet": "green",
-            "CuckooSet<Lazy>": "orange",
-            "CuckooSet<PrefetchOneLazy>": "orange",
-            "CuckooSet<Eager>": "pink",
-            "CuckooSet<PrefetchBothEager>": "pink",
-            "KphfSet<Sort>": "lime",
-            "KphfSet<SortBump>": "blue",
-            "KphfSet<SortBumpGreedy>": "cyan",
-            "FphDynSet": "purple",
-            "FphMetaSet": "black",
-            "PhfSet<PHast+>": "cyan",
-            "PhfSet<PHast+minimal>": "darkred",
-            "PhfSet<PtrHash>": "lime",
-        }
-        plt.close()
+            data["label"] = data.apply(
+                lambda row: row["h"] + " (" + str(row["alpha"]) + "x)", axis=1
+            )
 
-        titles = ["p=0.01", "p=0.50", "p=0.99"]
-        thread_counts = data["threads"].unique()
-        thread_counts = [1, max(thread_counts)]
-        sizes = [12 * 1024 * 1024]
-        cache_labels = ["L3  ", "  RAM"]
+            queries = ["q01", "q50", "q99"]
+            group_columns = [
+                "h",
+                "pf",
+                "threads",
+                "metric",
+                "n",
+                "alpha",
+                "label",
+                "kphf_target_bits_per_key",
+            ]
+            data = data.groupby(group_columns, as_index=False, sort=False)[
+                ["build", *queries]
+            ].min()
 
-        nrows = len(thread_counts)
-        ncols = len(queries)
-        fig, axes = plt.subplots(
-            nrows, ncols, figsize=(5 * ncols, 4 * nrows), sharey="row", sharex=True
-        )
+            labels = data["label"].unique()
+            palette = sns.color_palette(n_colors=len(labels))
+            label_color = {
+                "FxHashSet": "red",
+                "U64HashSet": "magenta",
+                "CuckooSet<Lazy>": "orange",
+                "CuckooSet<Eager>": "brown",
+                "FphDynSet": "pink",
+                "PhfSet<PHast+>": "lime",
+                "PhfSet<PtrHash>": "cyan",
+                "kPhfSet<kPtrHash>": "blue",
+                # "Hd8Set": "black",
+                # "MapEmbed": "brown",
+            }
+            plt.close()
 
-        for ri, threads in enumerate(thread_counts):
-            thread_data = data[data["threads"] == threads]
-            groups = thread_data.groupby(["h", "alpha"])
-            for ci, (q, title) in enumerate(zip(queries, titles)):
-                ax = axes[ri][ci]
+            def width(name, alpha):
+                if name != "kPhfSet<kPtrHash>":
+                    return 1.5
+                return {
+                    0.7: 1.5,
+                    0.9: 1.1,
+                    0.95: 0.8,
+                }[alpha]
 
-                for (h, alpha), subset in groups:
-                    sns.lineplot(
-                        data=subset,
-                        x="n",
-                        y=q,
-                        ax=ax,
-                        estimator=None,
-                        errorbar=None,
-                        color=label_color[h],
-                        lw=1.5,
-                        label=h + " " + str(alpha),
-                    )
-                if ri == 0:
-                    ax.set_title(title)
+            titles = ["p=0.01", "p=0.50", "p=0.99"]
+            # thread_counts = data["threads"].unique()
+            # thread_counts = [1, max(thread_counts)]
+            sizes = [12 * 1024 * 1024]
+            cache_labels = ["L3  ", "  RAM"]
 
-                ax.set_xlabel("n" if ri == nrows - 1 else "")
-                ax.set_xscale("log", base=2)
+            thread_counts = data["threads"].unique()
+            thread_counts = [1] if mode == "single" else [1, max(thread_counts)]
+            nrows = len(thread_counts)
+            ncols = len(queries)
+            fig, axes = plt.subplots(
+                nrows,
+                ncols,
+                figsize=(5 * ncols, 4 * nrows),
+                sharey="row",
+                sharex=True,
+                squeeze=False,
+            )
 
-                ax.grid(True, which="both", ls="--", lw=0.5)
+            for ri, threads in enumerate(thread_counts):
+                thread_data = data[data["threads"] == threads]
+                groups = thread_data.groupby(["h", "alpha"], sort=False)
+                for ci, (q, title) in enumerate(zip(queries, titles)):
+                    ax = axes[ri][ci]
 
-                ax.set_ylabel(f"threads={threads}\nns / query" if ci == 0 else "")
-                ax.set_ylim(0)
+                    for (h, alpha), subset in sorted(
+                        groups,
+                        key=lambda x: (
+                            list(label_color).index(x[0][0])
+                            if x[0][0] in label_color
+                            else len(label_color)
+                        ),
+                    ):
+                        sns.lineplot(
+                            data=subset,
+                            x="n",
+                            y=q,
+                            ax=ax,
+                            estimator=None,
+                            errorbar=None,
+                            color=label_color[h],
+                            lw=width(h, alpha),
+                            label=h + " " + str(alpha),
+                        )
+                    if ri == 0:
+                        ax.set_title(title)
 
-                if ri == 0 and ci == ncols - 1:
-                    ax.legend(loc="upper left", fontsize=8)
-                else:
-                    ax.legend().remove() if ax.get_legend() else None
+                    ax.set_xlabel("n" if ri == nrows - 1 else "")
+                    ax.set_xscale("log", base=2)
 
-        fig.suptitle(f"{name}: u{w} hashset query throughput ({metric})")
-        fig.tight_layout()
-        fig.savefig(f"plot-{name}-{metric}.png", bbox_inches="tight", dpi=300)
-        fig.savefig(f"plot-{name}-{metric}.pdf", bbox_inches="tight")
-        fig.savefig(f"plot-{name}-{metric}.svg", bbox_inches="tight")
+                    ax.grid(True, which="both", ls="--", lw=0.5)
+
+                    if mode == "single":
+                        ax.set_ylabel(f"ns / query" if ci == 0 else "")
+                    else:
+                        ax.set_ylabel(
+                            f"threads={threads}\nns / query" if ci == 0 else ""
+                        )
+                    ax.set_ylim(0)
+
+                    if ri == 0 and ci == 0:
+                        ax.legend(loc="upper left", fontsize=8)
+                    else:
+                        ax.legend().remove() if ax.get_legend() else None
+
+            metric_name = {
+                "loop": "for loop",
+                "prefetch": "prefetch (old)",
+                "prefetch2": "loop with prefetch",
+            }
+
+            cpu_name = {
+                "laptop": "Intel i7-10750H laptop",
+                "diffie": "AMD EPYC Zen 4 9684X server",
+                "floyd": "Intel Xeon Gold 6530 server",
+            }[name]
+
+            if mode == "single":
+                fig.suptitle(
+                    f"StaticHashset<u64> query throughput ({cpu_name}, 1 thread, loop with prefetch)"
+                )
+                fig.tight_layout()
+                fig.savefig(f"plot.png", bbox_inches="tight", dpi=300)
+                fig.savefig(f"plot.pdf", bbox_inches="tight")
+                fig.savefig(f"plot.svg", bbox_inches="tight")
+            else:
+                fig.suptitle(
+                    f"Hashset<u64> query throughput ({cpu_name}, {metric_name[metric]})"
+                )
+                fig.tight_layout()
+                fig.savefig(f"plot-{name}-{metric}.png", bbox_inches="tight", dpi=300)
+                fig.savefig(f"plot-{name}-{metric}.pdf", bbox_inches="tight")
+                fig.savefig(f"plot-{name}-{metric}.svg", bbox_inches="tight")
